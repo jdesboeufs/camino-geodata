604b8447585e662ead986e6f3c1b00ef6d7a82b5
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-08-23 16:56:05 +0200

    Import h outremer

--- import.js
+++ import.js
@@ -4,10 +4,12 @@ const {writeFileSync} = require('fs')
 const {concat} = require('lodash')
 const decorate = require('./lib/decorate')
 const importMetropoleH = require('./lib/import/h/metropole')
+const importOutremerH = require('./lib/import/h/outremer')
 
 async function main() {
   const titres = concat(
-    await importMetropoleH()
+    await importMetropoleH(),
+    await importOutremerH()
   )
   titres.forEach(decorate)
   const fc = {type: 'FeatureCollection', features: titres}
