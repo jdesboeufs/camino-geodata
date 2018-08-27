f756c6da86393195e68e4d7f7e5589e282e735e4
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-08-27 11:40:40 +0200

    Extraction points et périmètres JORF

--- import.js
+++ import.js
@@ -1,10 +1,11 @@
-#! /usr/bin/env node
+#! /usr/bin/env node --max_old_space_size=8192
 const {join} = require('path')
 const {writeFileSync} = require('fs')
 const {concat} = require('lodash')
 const decorate = require('./lib/decorate')
 const importMetropoleH = require('./lib/import/h/metropole')
 const importOutremerH = require('./lib/import/h/outremer')
+const extractPoints = require('./lib/points-jorf')
 
 async function main() {
   const titres = concat(
@@ -14,6 +15,9 @@ async function main() {
   titres.forEach(decorate)
   const fc = {type: 'FeatureCollection', features: titres}
   writeFileSync(join(__dirname, 'titres.geojson'), JSON.stringify(fc))
+
+  const titresJO = await extractPoints()
+  writeFileSync(join(__dirname, 'titres-jo.json'), JSON.stringify(titresJO))
 }
 
 main().catch(err => {
